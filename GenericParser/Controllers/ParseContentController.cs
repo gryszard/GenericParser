6c6f333ea8de5aa8a7d8137c939707b6c307f02f
gryszard <rysiupysiu234@gmail.com>
2026-07-28 14:50:13 +0200

    Add project files.

---
+++ GenericParser/Controllers/ParseContentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GenericParser.Controllers;
+
+[ApiController]
+[Route("/api/v1/parse-content")]
+public class ParseContentController : ControllerBase
+{
+    [HttpPost(Name = "ParseContent")]
+    public JsonResult ParseContent()
+    {
+        throw new NotImplementedException();
+    }
+}
